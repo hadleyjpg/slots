a50c6b49ca26e0b5a45497cbbf0ca04e594037eb
Hadley <hadleyslucher@gmail.com>
2024-05-10 15:14:25 -0500

    Node installs

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit money
+// 2. Determine number of lines to bet
+// 3. Collect bet amount
+// 4. Spin slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
